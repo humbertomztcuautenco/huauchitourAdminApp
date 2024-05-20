59bd1de200b95e8ae7225195902f5f4724c8f7ad
BraytoGBDX <lculpa45@gmail.com>
2024-05-20 16:46:38 -0600

    Reestructuracion de carpetas

---
+++ Screens/login.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const login = () => {
+  return (
+    <View>
+      <Text>login</Text>
+    </View>
+  )
+}
+
+export default login
+
+const styles = StyleSheet.create({})
