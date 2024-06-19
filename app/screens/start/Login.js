bd016cf82562d7978a7c1ea62f116d6f3db5a48a
BraytoGBDX <lculpa45@gmail.com>
2024-06-19 16:16:27 -0600

    STYLE: login layout

--- app/screens/start/Login.js
+++ app/screens/start/Login.js
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Platform, Linking } from 'react-native';
-import { Button, Input, Text, Icon } from 'react-native-elements';
-import { useNavigation } from '@react-navigation/native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import Api from '../../utils/Api';
-import Loader from '../../components/Loader';
-import Constants from 'expo-constants';
-import { useDispatch } from 'react-redux';
-import { addUser, selectEstab } from '../../features/auth/authSlice';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState } from "react";
+import { View, StyleSheet, Platform, Linking, ScrollView, Image, TextInput, TouchableOpacity, Modal } from "react-native";
+import { Button, Input, Text, Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import Api from "../../utils/Api";
+import Loader from "../../components/Loader";
+import Constants from "expo-constants";
+import { useDispatch } from "react-redux";
+import { addUser, selectEstab } from "../../features/auth/authSlice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Login({ navigation }) {
-
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
   const [verPassword, setVerPassword] = useState(false);
@@ -19,24 +18,33 @@ export default function Login({ navigation }) {
   const [errors, setErrors] = useState({});
   const navegacion = useNavigation();
 
+  const [modalVisible, setModalVisible] = useState(false);
+
   const handlePress = async () => {
     await Linking.openURL("mailto: correo@correo.com");
-  }
+  };
 
   const guardarValor = (name, value) => {
     setData({ ...data, [name]: value });
-  }
+  };
 
   const login = async () => {
     setErrors({});
-    let valoresAceptados = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let valoresAceptados =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (!data.password && !data.user) {
-      setErrors({ password: "Ingresa una contraseña", mail: "Ingresa un correo" });
+      setErrors({
+        password: "Ingresa una contraseña",
+        mail: "Ingresa un correo",
+      });
     } else if (!data.password) {
       setErrors({ password: "Ingresa una contraseña" });
       if (!data.user.match(valoresAceptados)) {
-        setErrors({ password: "Ingresa una contraseña", mail: "Ingresa una dirección de correo válido" });
+        setErrors({
+          password: "Ingresa una contraseña",
+          mail: "Ingresa una dirección de correo válido",
+        });
       }
     } else if (!data.user) {
       setErrors({ mail: "Ingresa un correo" });
@@ -45,144 +53,194 @@ export default function Login({ navigation }) {
     } else {
       setIsLoading(true);
       let api = new Api(`auth/adminestab`, `POST`, data);
-      await api.call().then(async res => {
+      await api.call().then(async (res) => {
         setTimeout(async () => {
           if (res.response) {
             setIsLoading(false);
-            console.log('Login successful:', res.result);
-            dispatch(addUser({
-              token: res.result.token,
-              user: res.result.id,
-              estabs: res.result.estabs,
-              id: res.result.id,
-              estabSelect: null,
-            })).then(async () => {
-              const storedToken = await AsyncStorage.getItem('token');
-              console.log('Token stored in AsyncStorage:', storedToken);
+            console.log("Login successful:", res.result);
+            dispatch(
+              addUser({
+                token: res.result.token,
+                user: res.result.id,
+                estabs: res.result.estabs,
+                id: res.result.id,
+                estabSelect: null,
+              })
+            ).then(async () => {
+              const storedToken = await AsyncStorage.getItem("token");
+              console.log("Token stored in AsyncStorage:", storedToken);
             });
 
             if (res.result.estabs.length > 1) {
-   
-              navigation.navigate('listestabs');
+              navigation.navigate("listestabs");
             } else {
               dispatch(selectEstab({ estabSelect: res.result.estabs[0] }));
-              navigation.navigate('home');
+              navigation.navigate("home");
             }
-
           } else {
             setIsLoading(false);
             setErrors({ password: res.message });
           }
-        }, 1)
-      })
+        }, 1);
+      });
     }
-  }
+  };
 
   return (
-    <KeyboardAwareScrollView style={{ height: '100%', backgroundColor: '#fff', paddingTop: Platform.OS == 'ios' ? Constants.statusBarHeight : 0 }} overScrollMode={'never'} keyboardShouldPersistTaps={'handled'}>
-      <View style={{ width: '100%', height: '85%', backgroundColor: "#fff" }} >
-        <Text style={{ marginTop: 90, marginLeft: 19, fontWeight: 'bold', fontSize: 25 }} allowFontScaling={false}>Hola bienvenido.</Text>
-        <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 80 }}>
-          <Input
-            placeholder="No. de teléfono o correo electrónico"
-            onChangeText={(value) => guardarValor('user', value)}
-            inputContainerStyle={{ borderBottomWidth: 0, justifyContent: 'center' }}
-            style={styles.inputStyle}
-            containerStyle={styles.inputContainer}
-            errorMessage={errors.mail}
+    <KeyboardAwareScrollView
+      style={styles.container}
+      overScrollMode={"never"}
+      keyboardShouldPersistTaps={"handled"}
+      contentContainerStyle={{ flexGrow: 1 }}
+    >
+      <Image
+        source={require("../../../assets/backLogin.jpg")}
+        style={styles.imageBackground}
+      />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.form}>
+          <Image
+            source={require("../../../assets/htlogo.png")}
+            style={styles.imgForm}
           />
-          <Input
+          <Text style={styles.welcomeText}>Bienvenido</Text>
+          <TextInput style={styles.input} placeholder="Correo" onChangeText={(value) => guardarValor('user', value)} />
+          <TextInput
+            style={styles.input}
             placeholder="Contraseña"
-            onChangeText={(value) => guardarValor('password', value)}
-            inputContainerStyle={{ borderBottomWidth: 0 }}
-            style={styles.inputStyle}
-            containerStyle={styles.inputContainer}
-            password={true}
             secureTextEntry={!verPassword}
-            rightIcon={
-              <Icon
-                type="font-awesome"
-                name={verPassword ? "eye-slash" : "eye"}
-                size={20}
-                iconStyle={{ color: "gray" }}
-                onPress={() => setVerPassword(!verPassword)}
-              />
-            }
-            errorMessage={errors.password}
+            onChangeText={(value) => guardarValor('password', value)}
           />
-          <Text
-            allowFontScaling={false}
-            style={{
-              color: "rgba(151,158,181,1)",
-              fontSize: 17,
-              marginBottom: 10,
-              textAlign: 'center'
-            }}>¿Dudas o aclaraciones? Contactanos:
-            <Text
-              allowFontScaling={false}
-              style={{
-                fontSize: 19,
-                fontWeight: 'bold',
-                textDecorationLine: 'underline',
-              }}
-              onPress={() => handlePress()}>
-              correo@correo.com
-            </Text>
+          <View style={styles.textsContainer}>
+            <Text style={styles.text}>Dudas o aclaraciones?</Text>
+            <Text style={styles.text}>Contactanos:</Text>
+            <TouchableOpacity>
+              <Text style={styles.contactText} onPress={handlePress}>correo@correo.com</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.forgotPasswordText} onPress={() => navegacion.navigate("recoverpassword")}>
+            ¿Olvidaste tu contraseña?
           </Text>
+          <TouchableOpacity style={styles.button} onPress={login}>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </TouchableOpacity>
         </View>
-        <View style={{ width: '100%' }}>
-          <Text
-            style={{ fontSize: 15, color: "black", textAlign: "center", fontWeight: 'bold', marginBottom: 20, marginTop: 10 }}
-            onPress={() => navegacion.navigate("recoverpassword")}
-          >¿Olvidaste tu contraseña?</Text>
-        </View>
+      </ScrollView>
 
-      </View>
-      <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 10 }}>
-        <Button
-          title="Entra"
-          titleStyle={{ fontSize: 20 }}
-          buttonStyle={styles.btnEntrar}
-          containerStyle={styles.btnContainer}
-          onPress={() => login()}
-        />
-      </View>
-      <Loader isVisible={isLoading} text='Cargando ...' />
+      <Loader isVisible={isLoading} text='Validando Correo...' />
+
+      <Modal
+        animationType="fadeIn"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(!modalVisible)}
+      >
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalIcon}></View>
+            <Text style={styles.modalTitle}>Validando Correo...</Text>
+          </View>
+        </View>
+      </Modal>
     </KeyboardAwareScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  estamosContentosDe: {
-    color: "rgba(151,158,181,1)",
-    fontSize: 16,
-    letterSpacing: -0.4
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'ios' ? Constants.statusBarHeight : 0,
   },
-  btnContainer: {
-    width: "90%",
-    height: 50,
-    fontSize: 25,
-    marginBottom: 20
-  },
-  btnEntrar: {
-    borderRadius: 6,
-    backgroundColor: "black",
-    fontSize: 25,
-    height: 45
-  },
-  inputContainer: {
-    width: "90%",
-    height: 55,
-    borderWidth: 1,
-    borderColor: "#fff",
-    borderRadius: 6,
-    backgroundColor: "#F0F0F0",
-    marginBottom: 30
-  },
-  inputStyle: {
+  scrollContainer: {
+    flexGrow: 1,
+    alignItems: 'center',
     justifyContent: 'center',
-    fontSize: 18,
-    marginBottom: 10,
-    paddingTop: 10
+  },
+  imageBackground: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  form: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80%",
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    borderRadius: 40,
+    padding: 20,
+  },
+  imgForm: {
+    width: "40%",
+    height: "18%",
+  },
+  welcomeText: {
+    color: "white",
+    fontSize: 35,
+    fontWeight: "800",
+    marginBottom: 30,
+  },
+  input: {
+    backgroundColor: "white",
+    width: "100%",
+    borderRadius: 20,
+    padding: 6,
+    paddingLeft: 15,
+    marginVertical: 10,
+  },
+  textsContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 50,
+  },
+  text: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 17,
+  },
+  contactText: {
+    fontSize: 17,
+    fontWeight: "900",
+    color: "white",
+  },
+  forgotPasswordText: {
+    color: "white",
+    fontSize: 17,
+    fontWeight: "800",
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: "#08A1F0",
+    borderRadius: 30,
+    paddingHorizontal: 80,
+    paddingVertical: 10,
+    marginBottom: 50,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    backgroundColor: 'white',
+    paddingVertical: 30,
+    paddingHorizontal: 50,
+    borderRadius: 20,
+    alignItems: 'center',
+  },
+  modalIcon: {
+    width: 80,
+    height: 80,
+    backgroundColor: "black",
+  },
+  modalTitle: {
+    fontSize: 25,
   }
 });
